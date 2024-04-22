c8e26edeca6478c16bceae3d26164dea1d07cd3f
juan perez <juan._099@hotmail.com>
2024-04-21 20:06:32 -0600

    Asigna asesores tabla

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ Route::get('/mercado_actual', function () {
     return view('mercado_actual');
 });
 
+Route::get('/inversiones', function () {
+    return view('inversiones.index');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
